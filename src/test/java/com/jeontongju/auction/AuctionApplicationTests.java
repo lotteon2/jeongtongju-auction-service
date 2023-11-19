92a41e3bce3d11dfceb22b464b643b127961a36b
최성훈 <33412452+realsuperman@users.noreply.github.com>
2023-11-19 16:12:47 +0900

    first commit

---
+++ src/test/java/com/jeontongju/auction/AuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeontongju.auction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
