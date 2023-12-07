75f2acd2736f65dc8e685bd9a077ded4298f1a5c
MJun111 <kmj0568@gmail.com>
2023-12-07 20:15:03 +0900

    FIX
    delete unused querydsl

--- src/main/java/com/jeontongju/auction/repository/BidInfoRepository.java
+++ src/main/java/com/jeontongju/auction/repository/BidInfoRepository.java
@@ -1,13 +1,12 @@
 package com.jeontongju.auction.repository;
 
 import com.jeontongju.auction.domain.BidInfo;
-import com.jeontongju.auction.repository.querydsl.BidInfoCustomRepository;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BidInfoRepository extends JpaRepository<BidInfo, Long>, BidInfoCustomRepository {
+public interface BidInfoRepository extends JpaRepository<BidInfo, Long> {
   List<BidInfo> findByConsumerId(Long consumerId);
   List<BidInfo> findAllByIsBidTrue();
 }
