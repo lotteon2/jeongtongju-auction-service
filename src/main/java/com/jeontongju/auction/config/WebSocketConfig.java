d3e50145827a769e7c3dc67db868b2de01699653
MJun111 <kmj0568@gmail.com>
2023-12-13 10:05:45 +0900

    FIX sockjs setting

--- src/main/java/com/jeontongju/auction/config/WebSocketConfig.java
+++ src/main/java/com/jeontongju/auction/config/WebSocketConfig.java
@@ -1,12 +1,17 @@
 package com.jeontongju.auction.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSocketMessageBroker
@@ -21,7 +26,17 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
     registry.addEndpoint("/chat")   //SockJS 연결 주소
-        .setAllowedOrigins("*");
-    // 주소 : ws://localhost:8080/chat
+        .setAllowedOrigins("*")
+        .withSockJS();
+  }
+
+  @EventListener
+  public void connectEvent(SessionConnectEvent sessionConnectEvent){
+    log.info("연결 성공, {}", sessionConnectEvent);
+  }
+
+  @EventListener
+  public void onDisconnectEvent(SessionDisconnectEvent sessionDisconnectEvent) {
+    log.info("연결 해제, {}", sessionDisconnectEvent);
   }
 }
