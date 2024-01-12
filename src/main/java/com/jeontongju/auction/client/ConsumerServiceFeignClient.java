90dde906a7cb1d03bff11fad7e315d41deea9e0b
MJun111 <kmj0568@gmail.com>
2024-01-12 14:57:24 +0900

    FIX
    modify Patch Mapping to Put Mapping
    cause Feign Client Not Supported Patch Mapping

--- src/main/java/com/jeontongju/auction/client/ConsumerServiceFeignClient.java
+++ src/main/java/com/jeontongju/auction/client/ConsumerServiceFeignClient.java
@@ -4,13 +4,13 @@ import io.github.bitbox.bitbox.dto.ConsumerInfoDto;
 import io.github.bitbox.bitbox.dto.FeignFormat;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @FeignClient(name = "consumer-service")
 public interface ConsumerServiceFeignClient {
   @GetMapping("/consumers/{consumerId}/auction")
   FeignFormat<ConsumerInfoDto> getConsumerInfo(@PathVariable Long consumerId);
-  @PatchMapping("/consumers/{consumerId}/credit/{deductionCredit}")
+  @PutMapping("/consumers/{consumerId}/credit/{deductionCredit}")
   FeignFormat<Boolean> deductCredit(@PathVariable Long consumerId, @PathVariable Long deductionCredit);
 }
