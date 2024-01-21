3a28cf05e8ca7ad0015debbf94f5cca61a5a20ee
MJun111 <kmj0568@gmail.com>
2024-01-21 17:22:24 +0900

    FIX
    add not null

--- src/main/java/com/jeontongju/auction/util/Mosaic.java
+++ src/main/java/com/jeontongju/auction/util/Mosaic.java
@@ -2,7 +2,7 @@ package com.jeontongju.auction.util;
 
 public class Mosaic {
   public static String nameMosaic(String str) {
-    if (str.length() > 2) {
+    if (str != null && str.length() > 2) {
       str = str.replace(str.charAt(1), '*');
     }
     return str;
